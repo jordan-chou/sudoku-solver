627d49763a0e1a56426027ad0deb33b3510bc8fb
Jordan Chou <jordan.chou@live.ca>
2020-01-16 16:47:20 -0500

    Refactored Panels

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,5 +1,12 @@
+/*
+ * Jordan Chou
+ * 
+ * github.com/jordan-chou
+ * 
+ * Main program
+ */
+
 import javax.swing.*;
-import java.awt.*;
 
 class SudokuSolver {
     public static void main(String[] args) {
@@ -7,28 +14,9 @@ class SudokuSolver {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
 
-        // Creating panel
-        JPanel sudokuGridPanel = new JPanel(new GridBagLayout());
+        SudokuPanel sudokuPanel = new SudokuPanel();
 
-        // Creating 2D array of TextFields
-        JTextField[][] grid = new JTextField[3][3];
-
-        // Setting constraints of GridBag
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.anchor = GridBagConstraints.CENTER;
-        
-        // Adding label and textfield to Panel
-        int num = 1;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                constraints.gridx = j;
-                constraints.gridy = i;
-                grid[i][j] = new JTextField(Integer.toString(num++));
-                sudokuGridPanel.add(grid[i][j], constraints);
-            }
-        }
-
-        frame.getContentPane().add(sudokuGridPanel);
+        frame.getContentPane().add(sudokuPanel.getPanel());
         frame.setVisible(true);
     }
 }
