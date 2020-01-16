664d5443a9805590af27b72d96f8bd9aa80da582
Jordan Chou <jordan.chou@live.ca>
2020-01-16 14:32:47 -0500

    Created Grid Layout for UI

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -8,15 +8,25 @@ class SudokuSolver {
         frame.setSize(300, 300);
 
         // Creating panel
-        JPanel sudokuGridPanel = new JPanel(new BorderLayout());
+        JPanel sudokuGridPanel = new JPanel(new GridBagLayout());
 
-        // Creating label with textfield
-        JLabel label = new JLabel("Enter number:");
-        JTextField number = new JTextField(5);
+        // Creating 2D array of TextFields
+        JTextField[][] grid = new JTextField[3][3];
 
+        // Setting constraints of GridBag
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.CENTER;
+        
         // Adding label and textfield to Panel
-        sudokuGridPanel.add(label, BorderLayout.NORTH);
-        sudokuGridPanel.add(number, BorderLayout.SOUTH);
+        int num = 1;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                constraints.gridx = j;
+                constraints.gridy = i;
+                grid[i][j] = new JTextField(Integer.toString(num++));
+                sudokuGridPanel.add(grid[i][j], constraints);
+            }
+        }
 
         frame.getContentPane().add(sudokuGridPanel);
         frame.setVisible(true);
