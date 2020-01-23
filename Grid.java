67cb3882f1f627617b8f26c69630d539d5996faf
Jordan Chou <jordan.chou@live.ca>
2020-01-23 16:09:28 -0500

    Organized code, specified imports, and modified appearance

--- Grid.java
+++ Grid.java
@@ -6,8 +6,13 @@
  * Defines one 3x3 grid. The SudokuPanel will have 9 of these
  */
 
- import javax.swing.*; 
- import java.awt.*; 
+ import javax.swing.JPanel;
+ import javax.swing.JTextField;
+ import java.awt.Font;
+ import java.awt.Dimension; 
+ import java.awt.GridBagConstraints;
+ import java.awt.GridBagLayout;
+ import java.awt.Color; 
 
  public class Grid {
     private JPanel grid;
@@ -29,10 +34,17 @@
             for (int j = 0; j < numbers[i].length; j++) {
                 constraints.gridx = j;
                 constraints.gridy = i;
+
+                // Setting behavior of the square
                 numbers[i][j] = new JTextField(Integer.toString(num++));
                 numbers[i][j].setEditable(false);
                 numbers[i][j].setPreferredSize(new Dimension(30,30));
                 numbers[i][j].setHorizontalAlignment(JTextField.CENTER);
+
+                // Setting apperance of the square
+                numbers[i][j].setBackground(Color.WHITE);   // Sets to WHITE background
+                numbers[i][j].setFont(numbers[i][j].getFont().deriveFont(Font.BOLD, 14f));  // Sets to BOLD and size 14, using the current style
+
                 grid.add(numbers[i][j], constraints);
             }
         }
