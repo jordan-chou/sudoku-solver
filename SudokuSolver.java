f5020a4b5dd586164cafa925c45a41888395b464
Jordan Chou <jordan.chou@live.ca>
2020-01-23 16:18:13 -0500

    Specified imports and refactored frame into two panels

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -6,21 +6,36 @@
  * Main program
  */
 
-import javax.swing.*;
-import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import java.awt.GridLayout;
 
 class SudokuSolver {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Sudoku Solver");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 400);
+     
 
         SudokuPanel sudokuPanel = new SudokuPanel();
+        ButtonPanel buttonPanel = new ButtonPanel();
 
-        JPanel leftPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        leftPanel.add(sudokuPanel.getPanel());
+        // Creating Game Board 
+        JPanel gameBoard = new JPanel();
+        gameBoard.add(sudokuPanel.getPanel());
+        gameBoard.setLayout(new BoxLayout(gameBoard, BoxLayout.Y_AXIS));    // Centers the vertical alignment
 
-        frame.getContentPane().add(leftPanel);
+        // Creating Button Panel
+        JPanel rightPanel = new JPanel();
+        rightPanel.add(buttonPanel.getPanel());
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));  // Centers the vertical alignment
+
+
+        
+        frame.getContentPane().add(gameBoard);
+        frame.getContentPane().add(rightPanel);
+        frame.setLayout(new GridLayout(1,2));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 400);
         frame.setVisible(true);
     }
 }
